317f5a556488eb05d1c1a6d58e3c1a28330e24a7
jos-moerkerken <120014025+jos-moerkerken@users.noreply.github.com>
2023-12-10 15:04:50 +0100

    10-12 Adjustments

--- site/templates/project-2.php
+++ site/templates/project-2.php
@@ -46,20 +46,20 @@
 		<!-- mobile content -->
 		<div class="mobileContent">
 			<div class="mobilePhoto"></div>
-			<h1>Red</h1>
-			<p>Red is a color often associated with strong emotions such as passion, love, and anger. It's a bold and attention-grabbing color that can evoke feelings of excitement, warmth, and energy.</p>
+			<h3><?= $page->scrollTitleMobile1() ?></h3>
+			<p><?= $page->scrollTextMobile1() ?></p> 			
+			
+			<div class="mobilePhoto"></div>
+			<h3><?= $page->scrollTitleMobile2() ?></h3>
+			<p><?= $page->scrollTextMobile2() ?></p> 
 
 			<div class="mobilePhoto"></div>
-			<h1>Green</h1>
-			<p>Green is a color that is often associated with nature, growth, and harmony. It is a calming and relaxing color that can evoke feelings of balance, stability, and freshness. In color psychology, green is said to represent balance and stability, making it a popular choice for branding and marketing in the health and wellness industry. </p>
+			<h3><?= $page->scrollTitleMobile3() ?></h3>
+			<p><?= $page->scrollTextMobile3() ?></p> 
 
 			<div class="mobilePhoto"></div>
-			<h1>Pink</h1>
-			<p>Pink is a color that is often associated with femininity, romance, and sweetness. It is a softer and more delicate shade of red that can evoke feelings of warmth, love, and nurturing. In the world of branding and marketing, pink is often used to target a female audience or to promote products that are associated with beauty, love, or romance.</p>
-
-			<div class="mobilePhoto"></div>
-			<h1>Blue</h1>
-			<p>Blue is a color that is often associated with calmness, trust, and reliability. It is a peaceful and serene color that can evoke feelings of stability, security, and professionalism. In color psychology, blue is said to represent loyalty and trust, making it a popular choice for branding and marketing in the finance and technology industries.</p>
+			<h3><?= $page->scrollTitleMobile4() ?></h3>
+			<p><?= $page->scrollTextMobile4() ?></p> 
 		</div>
 
 		<!-- desktop content -->
